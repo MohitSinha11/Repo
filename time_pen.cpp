b757278142644b7b35edada9d78bb45ff5771671
Mohit Sinha <mohitsinha@Mohits-MacBook-Air.local>
2025-02-13 18:52:08 +0530

    time penalty added🫡

---
+++ time_pen.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// void time_penelty()
+// {
+//     int x, y;
+//     cin >> x >> y;
+
+//     cout << x + (y * 10);
+// }
+
+void solu()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        a.push_back(x);
+    }
+    int l = 0 , r = 1;
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        
+        
+
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        solu();
+    }
+}
